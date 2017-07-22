34d13c9e14ef2732bc80fc4dd52560733bcc7b5a
Ferdinand von den Eichen <ferdinand@vondeneichen.email>
2017-07-22 13:32:36 +0200

    Add HttpRequestService

---
+++ Assets/Project/src/http/HttpRequestService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Traitorstown.src;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class HttpRequestService : MonoBehaviour {
+
+    public string token = ""; 
+
+    public void createNewGame()
+    {
+        Debug.Log("Creating new game...");
+        UnityWebRequest request = UnityWebRequest.Get(Configuration.API_URL + Endpoints.POST_GAME);
+        StartCoroutine(makeRequest(request));
+    }
+
+    private IEnumerator makeRequest(UnityWebRequest request)
+    {
+        request.SetRequestHeader("token", token);
+
+        yield return request.Send();
+
+        if (request.isNetworkError)
+        {
+            Debug.Log("Error: " + request.error);
+        }
+        else
+        {
+            Debug.Log("Received " + request.downloadHandler.text);
+        }
+    }
+}
---
+++ Assets/SmartButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SmartButton : MonoBehaviour, IPointerDownHandler {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Hit IPointerDownHandler.OnPointerDown");
+    }
+}
